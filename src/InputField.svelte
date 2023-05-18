3f975812b329df923d57c0db01cfadffa1e01f31
deepikapani <deepikapani2001@gmail.com>
2023-05-18 08:51:19 +0530

    Adding bindin project

---
+++ src/InputField.svelte
@@ -0,0 +1,11 @@
+<!-- Binding to component instance -->
+
+<script>
+	import InputField from './InputField.svelte';
+Input field svelte
+	let field;
+</script>
+
+<InputField bind:this={field}/>
+
+<button on:click={() => field.focus()}>Focus field</button>
